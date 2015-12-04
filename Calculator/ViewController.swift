defc2ee25f60d0b49b03f5ae1849af37a5655444
Mark Nichols <mark@zanshin.net>
2015-12-04 13:40:27 -0600

    lecture 1 partially completed

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+class ViewController: UIViewController
+{
+    // The Calculator display
+    @IBOutlet weak var display: UILabel!
+    
+    
+    
 }
 
